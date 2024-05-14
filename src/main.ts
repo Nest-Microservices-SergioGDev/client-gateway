95e00171e06732ae3005a1c5c608b85b8e7e3deb
SergioGDev <admin@sergiogdev.com>
2024-05-14 18:41:46 +0200

    fix: removed console log from main

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
-  console.log('Gatito')
-
   await app.listen(envs.port);
 
   logger.log(`Gatewey running on port ${envs.port}`);
